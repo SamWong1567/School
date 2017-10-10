574a6b29aa2886c857752dfc2010bbf1f27ddc9e
SamWong1567 <sam.1567@hotmail.com>
2017-10-10 21:39:25 +0800

    10/10 Save 3
    Added a panel
    Added a Game Manager (Game Object)
    2 scripts
    Game Manager & Pseduocodes

---
+++ Sam_FYP_Prototype/Assets/GameManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public PseudoCodes[] pArray;
+   
+    //called whenever the game is restared or scene is reloaded
+    //
+    void Start()
+    {
+
+    }
+}
