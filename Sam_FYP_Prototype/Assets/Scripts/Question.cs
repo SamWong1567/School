7157365ebad635c6f1cd219e633851a1ff83324c
SamWong1567 <sam.1567@hotmail.com>
2017-10-11 12:33:28 +0800

    11/10 Save
    Added a Question class with the var:
    - questions
    - answer
    - wrongAns
    
    Added a TFQuizHandler class for the TFQuestions
    
    Rename StartScene to TFScene

---
+++ Sam_FYP_Prototype/Assets/Scripts/Question.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour {
+
+    public Question(string q, string a, string wa)
+    {
+        question = q;
+        answer = a;
+        wrongAns = wa;
+    }
+
+    public string question;
+    public string answer;
+    public string wrongAns;
+   
+}
---
+++ Sam_FYP_Prototype/Assets/Scripts/TFQuizHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System; //for try catch blocks
+using System.IO; //for StreamReader
+
+//This class handles the true and false questions
+public class TFQuizHandler : MonoBehaviour
+{ 
+	// Use this for initialization
+	void Start ()
+    {
+        //list of Question objects
+        List<Question> qnsList = new List<Question>();
+        //temporary store for the string that is read in line by line from the text file
+        string line = "";
+        //temporary stre for elements of a string split
+        string[] tempArray;
+        // temporary store for question
+        string q;
+        // temporary store for answer
+        string a;
+        // temporary store for wrong answer
+        string wa;
+
+        //get relative path to file as opposed to abosolute so that the file can be read on any computer
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "TFQuestions.txt");
+
+        try
+        {
+            //Read text file and extract it's contents line by line
+            StreamReader sr = new StreamReader(filePath);
+           
+            while((line = sr.ReadLine())!= null)
+            {
+                
+                //split contents in file with the delimiter ','
+                tempArray = line.Split(',');
+                q = tempArray[0];
+                a = tempArray[1];
+                wa = tempArray[2];
+            //creating and adding instance of Question objects to qnsList  
+            qnsList.Add(new Question(q, a,wa));
+                /* for printing 
+                int counter = 0;
+                print(qnsList[counter].question);
+                print(qnsList[counter].answer);
+                print(qnsList[counter].wrongAns);
+                counter++;
+                */
+                
+            }
+            sr.Close();
+        }
+        catch(Exception e)
+        {
+            Debug.LogException(e, this);
+        }
+
+    }
+
+}
