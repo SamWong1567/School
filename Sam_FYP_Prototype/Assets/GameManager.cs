d574e9ab5b5b493d18f1f5fea584d41ea9667e34
SamWong1567 <sam.1567@hotmail.com>
2017-11-09 19:20:01 +0800

    Revert "13 /10 Save 1"
    This reverts commit 507f218621cb180e9bce3c61f1af511a980e921a.

--- Sam_FYP_Prototype/Assets/GameManager.cs
+++ Sam_FYP_Prototype/Assets/GameManager.cs
@@ -26,8 +26,7 @@ public class GameManager : MonoBehaviour {
     private Text QnsText;
 
     Boolean clearStage = false;
-
-    //counter to display every subsequent question
+    //counter for changing questions to display
     static int counter = 0;
 
     //delay when changing question
@@ -42,11 +41,11 @@ public class GameManager : MonoBehaviour {
         try
         {
             //Read text file and extract it's contents line by line
-            //The contents store the question as well as the answers
             StreamReader sr = new StreamReader(filePath);
 
             while ((line = sr.ReadLine()) != null)
             {
+
                 //split contents in file with the delimiter ','
                 tempArray = line.Split(',');
                 q = tempArray[0];
@@ -62,7 +61,7 @@ public class GameManager : MonoBehaviour {
         {
             Debug.LogException(e, this);
         }
-        //Proceed to display question after loading in the questions
+
         displayQuestion();
         
     }
@@ -86,7 +85,7 @@ public class GameManager : MonoBehaviour {
         checkAnswer(a);
     }
 
-    //check if answer chosen by user is correct or wrong
+    //check if answer is correct or wrong
     public void checkAnswer(String ans)
     {
         if(ans.Equals(qnsList[counter].answer))
---
+++ Sam_FYP_Prototype/Assets/Scripts/PseudoCodes.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//notify unity that this class will be saving and storing information
+//After which, we can edit values in the inspector
+[System.Serializable]
+public class PseudoCodes {
+
+    public string pCode;
+    
+
+}
